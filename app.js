91990a1b030ec938b1e0214182089c4b3b932874
Jaendre <88118631+Jaendre@users.noreply.github.com>
2026-09-06 11:42:45 +0200

    Restore app.js after accidental stub

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-SEE_FILE
+(function () {
+    var script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/gh/Jaendre/cruisenext-studio-plus@33200f534f24337d88ac4da351c0fc4fecd5a811/app.js";
+    document.documentElement.appendChild(script);
+})();
